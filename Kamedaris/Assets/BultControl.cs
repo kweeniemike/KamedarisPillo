9e47688ee42e6504e2211a417507cca73f808941
Maikel Janssen <kweenie_mike@hotmail.com>
2015-08-21 22:49:47 +0200

    unity project added

---
+++ Kamedaris/Assets/BultControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BultControl : MonoBehaviour {
+
+	public GameObject bult01;
+	public float value = 0f;
+	private float prevValue = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (value != prevValue) {
+
+		}
+	bult01.
+	}
+}
