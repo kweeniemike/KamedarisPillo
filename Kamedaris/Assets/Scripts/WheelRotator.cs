2ce9e42872b2dd697554832723ca3a31aca60139
SanderSwi <157578ss>
2015-08-22 20:31:34 +0200

    wheelrotate + screenshake script

---
+++ Kamedaris/Assets/Scripts/WheelRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelRotator : MonoBehaviour {
+
+	public float speed = 50f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0,0 , speed* Time.deltaTime));
+	}
+}
