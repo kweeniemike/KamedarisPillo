b1f54fff3be0a9cbf25018429698b0032781b3e0
Maikel Janssen <kweenie_mike@hotmail.com>
2015-08-22 14:35:11 +0200

    moving goes up and down instead of scale changing

--- Kamedaris/Assets/Scripts/BultControl.cs
+++ Kamedaris/Assets/Scripts/BultControl.cs
@@ -8,18 +8,27 @@ public class BultControl : MonoBehaviour {
 
 	public float newRotValue = 0f;
 	public Player playerInputScript;
-	public float exMin = 0.5f;
+	public float exMin = 0f;
 	public float exMax = 1.5f;
+	private float moveMin = 0f;
+	private float moveMax = 1.5f;
 
 	private float currentRotValue = 0f;
 	private bool rotating = false;
 
-	public float newExValue = 0.5f;
-	public float newExValue2 = 0.5f;
-	private float currentExValue = 0.5f;
-	private bool expanding = false;
-	private float currentExValue2 = 0.5f;
-	private bool expanding2 = false;
+	public float newExValue = 0f;
+	public float newExValue2 = 0f;
+	private float currentExValue = 0f;
+	//private bool expanding = false;
+	private float currentExValue2 = 0f;
+	//private bool expanding2 = false;
+
+	private float newMoveValue = -1f;
+	private float newMoveValue2 = -1f;
+	private float currentMoveValue = -1f;
+	//private bool expanding = false;
+	private float currentMoveValue2 = -1f;
+	//private bool expanding2 = false;
 
 	private Vector3 v3ToRot = Vector3.zero;
 	private Vector3 v3CurrentRot;
@@ -28,15 +37,17 @@ public class BultControl : MonoBehaviour {
 	//private Vector3 v3ToEx = Vector3.zero;
 	//private Vector3 v3CurrentEx;
 	private float speedEx = 5f;
+	private float speedMove = 0f;
 
 	// Use this for initialization
 	void Start () {
-		v3CurrentRot = bult01.transform.eulerAngles;
+		//v3CurrentRot = bult01.transform.eulerAngles;
 		//v3CurrentEx = bult01.transform.localScale;
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		/*
 		if (playerInputScript.pilloPressure1 > 0.005f) {
 			newExValue = Mathf.Clamp (playerInputScript.pilloPressure1 * (exMax - exMin)*1.5f, exMin, exMax);
 		} else {
@@ -48,17 +59,46 @@ public class BultControl : MonoBehaviour {
 		else {
 			newExValue2 = exMin;
 		}
+
 		//newExValue = Mathf.Clamp (playerInputScript.pilloPressure1, exMin, exMax);
 		//newExValue2 = Mathf.Clamp (playerInputScript.pilloPressure2, exMin, exMax);
 		//Debug.Log (newExValue + ";" + expanding);
+
 		if (Mathf.Abs(newExValue - currentExValue) > 0.01f) {
 			currentExValue = newExValue;
-			StartCoroutine(Expand(currentExValue, bult01, 1));
+			StartCoroutine(Expand(currentExValue, bult01));
 		}
 		if (Mathf.Abs(newExValue2 - currentExValue2) > 0.01f) {
 			currentExValue2 = newExValue2;
-			StartCoroutine(Expand(currentExValue2, bult02, 2));
+			StartCoroutine(Expand(currentExValue2, bult02));
 		}
+		/*/
+
+		//if (playerInputScript.pilloPressure1 > 0.005f) {
+			newMoveValue = Mathf.Clamp (playerInputScript.pilloPressure1 * (moveMax - moveMin), moveMin, moveMax);
+		//} else {
+			//newMoveValue = moveMin;
+		//}
+		//if (playerInputScript.pilloPressure2 > 0.005f) {
+		newMoveValue2 = Mathf.Clamp (playerInputScript.pilloPressure2 * (moveMax - moveMin), moveMin, moveMax);
+		//}
+		//else {
+			//newMoveValue2 = moveMin;
+		//}
+		
+		if (Mathf.Abs(newMoveValue - currentMoveValue) > 0.01f) {
+
+			currentMoveValue = newMoveValue;
+			StartCoroutine(MoveUpDown(currentMoveValue, bult01));
+		}
+		if (Mathf.Abs(newMoveValue2 - currentMoveValue2) > 0.01f) {
+
+			currentMoveValue2 = newMoveValue2;
+			StartCoroutine(MoveUpDown(currentMoveValue2, bult02));
+		}
+		//*/
+
+
 		//Debug.Log ("1" + expanding + "-2" + expanding2);
 		/*if (newRotValue != currentRotValue && !rotating) {
 			currentRotValue = newRotValue;
@@ -78,10 +118,10 @@ public class BultControl : MonoBehaviour {
 			bult.transform.eulerAngles = v3CurrentRot; 
 			yield return null;
 		}
-		rotating = false;
+		//rotating = false;
 	}
 
-	IEnumerator Expand(float size, GameObject bult, int number)
+	IEnumerator Expand(float size, GameObject bult)
 	{
 		//Debug.Log(Vector3.Distance(v3ToEx, v3CurrentEx));
 		Vector3 v3ToEx = new Vector3 (bult.transform.localScale.x, size, bult.transform.localScale.z);
@@ -91,7 +131,20 @@ public class BultControl : MonoBehaviour {
 			bult.transform.localScale = v3CurrentEx; 
 			yield return new WaitForFixedUpdate();
 		}
-		expanding = false;
+		//expanding = false;
 		//expanding2 = false;
+	}
+
+	IEnumerator MoveUpDown(float position, GameObject bult)
+	{
+		Vector3 v3ToMove = new Vector3 (bult.transform.position.x, position, bult.transform.position.z);
+		Vector3 v3CurrentMove = bult.transform.position;
+		while (Vector3.Distance(v3ToMove, v3CurrentMove) > 0.005f) {
+			bult.GetComponent<Rigidbody> ().MovePosition (v3ToMove); 
+			yield return new WaitForFixedUpdate();
+		}
+
+		//Vector3 moveVector = new Vector3 (bult.transform.position.x, position, bult.transform.position.z);
+		//bult.GetComponent<Rigidbody> ().MovePosition (moveVector);
 	}
 }
