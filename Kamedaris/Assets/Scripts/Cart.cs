4ea65278871a57ab2c8f82b86c9462b642111cd4
Aldwoni(Pim Rutten) <aldwoni@gmail.com>
2015-08-22 15:30:52 +0200

    trigger only once

--- Kamedaris/Assets/Scripts/Cart.cs
+++ Kamedaris/Assets/Scripts/Cart.cs
@@ -1,53 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-
-public class Cart : MonoBehaviour {
-	public enum cartColor{green, brown};
+public class Cart : MonoBehaviour
+{
+	public enum cartColor
+	{
+		green,
+		brown}
+	;
 	public cartColor color;
 	public Score score;
 	public FallingObjectCreator objectcounter;
+	public List<GameObject> uniqueObjects;
 
-	void OnTriggerEnter(Collider collider)
+	void OnTriggerEnter (Collider collider)
 	{
 		GameObject opponent = collider.gameObject;
-		if(opponent.tag ==  "Meloen" && color == cartColor.green)
-		{
-			if(opponent.name.StartsWith("Gouden"))
-			{
-				score.addSpecialScore();
+		if (AddToList(opponent)) {
+			opponent.GetComponent<SelfDestruction>().StartTimer();
+			if (opponent.tag == "Meloen" && color == cartColor.green) {
+				if (opponent.name.StartsWith ("Gouden")) {
+					score.addSpecialScore ();
+				} else {
+					score.addScore ();
+				}
+				objectcounter.goldMelonCounter++;
+				//Destroy(opponent.transform.parent.gameObject);
+			} else if (opponent.tag == "Kokosnoot" && color == cartColor.brown) {
+				if (opponent.name.StartsWith ("Gouden")) {
+					score.addSpecialScore ();
+				} else {
+					score.addScore ();
+				}
+				objectcounter.goldKokosnootCounter++;
+				//Destroy(opponent.transform.gameObject);
+			} else if (opponent.tag == "Kokosnoot" && color == cartColor.green) {
+				score.negativeScore ();
+				objectcounter.goldMelonCounter = 0;
+				//Destroy(opponent.transform.gameObject);
+			} else if (opponent.tag == "Meloen" && color == cartColor.brown) {
+				score.negativeScore ();
+				objectcounter.goldKokosnootCounter = 0;
+				//Destroy(opponent.transform.parent.gameObject);
 			}
-			else
-			{
-				score.addScore();
-			}
-			objectcounter.goldMelonCounter++;
-			//Destroy(opponent.transform.parent.gameObject);
 		}
-		else if(opponent.tag == "Kokosnoot" && color == cartColor.brown)
-		{
-			if(opponent.name.StartsWith("Gouden"))
-			{
-				score.addSpecialScore();
-			}
-			else
-			{
-				score.addScore();
-			}
-			objectcounter.goldKokosnootCounter++;
-			//Destroy(opponent.transform.gameObject);
-		}
-		else if(opponent.tag == "Kokosnoot" && color == cartColor.green)
-		{
-			score.negativeScore();
-			objectcounter.goldMelonCounter=0;
-			//Destroy(opponent.transform.gameObject);
-		}
-		else if(opponent.tag == "Meloen" && color == cartColor.brown)
-		{
-			score.negativeScore();
-			objectcounter.goldKokosnootCounter=0;
-			//Destroy(opponent.transform.parent.gameObject);
+	}
+
+	bool AddToList (GameObject g)
+	{
+		if (uniqueObjects.Contains (g)) {
+			return false;
+		} else {
+			uniqueObjects.Add (g);
+			return true;
 		}
 	}
 }
