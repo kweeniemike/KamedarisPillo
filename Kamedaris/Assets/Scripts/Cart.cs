5400b6741c0d895c01075d31397d5fcdb5d83bc9
Aldwoni(Pim Rutten) <aldwoni@gmail.com>
2015-08-22 02:33:34 +0200

    add kar zodat objecten geremoved worden

---
+++ Kamedaris/Assets/Scripts/Cart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Cart : MonoBehaviour {
+	public enum cartColor{red, blue};
+	public cartColor color;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject opponent = collision.collider.gameObject;
+		if(opponent.tag ==  "Meloen" && color == cartColor.red)
+		{
+			//adscore
+		}
+		else if(opponent.tag == "Kokosnoot" && color == cartColor.blue)
+		{
+			//addscore
+		}
+		else
+		{
+			//addNegativeScore
+		}
+		Destroy(opponent);
+	}
+}
