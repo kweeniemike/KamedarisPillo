9e9a410b2c648b75c416c70b6338df0cbf83134a
Maikel Janssen <kweenie_mike@hotmail.com>
2015-08-23 10:35:43 +0200

    Soundmanager setup - nog niet werkend

---
+++ Kamedaris/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//Method for playing songs by name for once
+	//Method for playing the theme music and not?
+	//List of sounds and when they are played:
+
+}
