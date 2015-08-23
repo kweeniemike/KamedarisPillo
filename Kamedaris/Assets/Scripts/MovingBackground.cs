f421a287e31abea40f78c549703b817bc31f9337
Aldwoni(Pim Rutten) <aldwoni@gmail.com>
2015-08-23 12:28:41 +0200

    new buttons, positions will be fixed

---
+++ Kamedaris/Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingBackground : MonoBehaviour {
+	public float horizontalSpeed = 0.5f;
+	public float verticalSpeed = 0f;
+	public Renderer r;
+
+	void Awake()
+	{
+		r = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2(Time.time * horizontalSpeed,Time.time * verticalSpeed);
+		r.material.mainTextureOffset = offset;
+	}
+}
