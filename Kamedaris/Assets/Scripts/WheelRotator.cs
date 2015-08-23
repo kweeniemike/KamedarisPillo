97866eeaed4be864bc67443f20ce6fdad4760215
Maikel Janssen <kweenie_mike@hotmail.com>
2015-08-23 13:56:39 +0200

    changed volume and did stuff. Also squeecky wheels

--- Kamedaris/Assets/Scripts/WheelRotator.cs
+++ Kamedaris/Assets/Scripts/WheelRotator.cs
@@ -6,11 +6,23 @@ public class WheelRotator : MonoBehaviour {
 	public float speed = 50f;
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine (SqueeckyWheels());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (new Vector3 (0,0 , speed* Time.deltaTime));
+	}
+
+	IEnumerator SqueeckyWheels()
+	{
+		int seconds = Random.Range (8, 15 + 1);
+		while (!Score.gameEnded) {
+			string clipName = "Squeek0" + Random.Range (1, 3).ToString ();
+			SoundManager.PlayClipOnce (clipName, 0.5f);
+
+			yield return new WaitForSeconds(seconds);
+			seconds = Random.Range(8, 15 + 1);
+		}
 	}
 }
