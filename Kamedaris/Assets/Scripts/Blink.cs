93eda63ad602dce9d9c7ca8203263b63a04c99ab
Aldwoni(Pim Rutten) <aldwoni@gmail.com>
2015-08-23 14:40:18 +0200

    bllink

---
+++ Kamedaris/Assets/Scripts/Blink.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour {
+	public Sprite blinkImage;
+	public Sprite unblinkImage;
+	private SpriteRenderer r;
+	public bool blink;
+	// Use this for initialization
+	void Start () {
+		blink=false;
+		r = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(blink)
+		{
+			r.sprite = blinkImage;
+			blink = false;
+		}
+		if(!blink)
+		{
+			int i = Random.Range(1,25);
+			if(i==7)
+			{
+				blink = true;
+			}
+			if(blink)
+			{
+				r.sprite = unblinkImage;
+			}
+		}
+	}
+}
